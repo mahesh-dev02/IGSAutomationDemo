3865cdb6eebf0e4266c7c2fced8c28d63b03b7d9
mahesh-dev02 <137702127+mahesh-dev02@users.noreply.github.com>
2024-01-19 11:39:37 +0530

    Test Suite Added

---
+++ Test Suites/DemoSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>73c2c755-2d78-41ff-aba0-b1dab766297a</testSuiteGuid>
+   <testCaseLink>
+      <guid>c43321c8-6f56-4480-88c7-73622509085d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
